9e80963356a9ccb0e41bee6bff1ca200cc9596ab
germanbogatov <bogatovgrmn@gmail.com>
2023-12-25 15:10:38 +0300

    feat(no-ref): test

---
+++ client.go
@@ -0,0 +1,30 @@
+package rabbitmq
+
+import (
+	"context"
+	"io"
+)
+
+type MessageQueue interface {
+	io.Closer
+	DeclareQueue(name string, durable, autoDelete, exclusive, noWait bool, args map[string]interface{}) error
+	DeclareExchange(name, kind string, durable, autoDelete, internal, noWait bool, args map[string]interface{}) error
+}
+
+type Producer interface {
+	MessageQueue
+	Publish(ctx context.Context, target string, body []byte) error
+}
+
+type Consumer interface {
+	MessageQueue
+	Consume(target string) (<-chan Message, error)
+	Ack(id uint64, multiple bool) error
+	Nack(id uint64, multiple bool, requeue bool) error
+	Reject(id uint64, requeue bool) error
+}
+
+type Message struct {
+	ID   uint64
+	Body []byte
+}
---
+++ producer.go
@@ -0,0 +1,64 @@
+package rabbitmq
+
+import (
+	"context"
+	"fmt"
+)
+
+type ProducerConfig struct {
+	BaseConfig
+}
+
+type rabbitMQProducer struct {
+	*rabbitMQBase
+}
+
+func NewRabbitMQProducer(cfg ProducerConfig) (Producer, error) {
+	producer := &rabbitMQProducer{
+		rabbitMQBase: &rabbitMQBase{
+			done: make(chan bool),
+		},
+	}
+
+	addr := fmt.Sprintf("amqp://%s:%s@%s:%s/", cfg.Username, cfg.Password, cfg.Host, cfg.Port)
+	err := producer.connect(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	go producer.handleReconnect(addr)
+
+	return producer, nil
+}
+
+func (r *rabbitMQProducer) Publish(ctx context.Context, target string, body []byte) error {
+	if !r.Connected() {
+		return errNotConnected
+	}
+
+	err := r.ch.PublishWithContext(
+		ctx,
+		"",
+		target,
+		false,
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         body,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to publish message due %v", err)
+	}
+
+	return nil
+}
+
+func (r *rabbitMQProducer) Close() error {
+	if err := r.close(); err != nil {
+		return err
+	}
+
+	return nil
+}
